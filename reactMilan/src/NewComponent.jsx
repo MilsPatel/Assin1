adf49c2965daed9567bf9f6bafdd0480816541da
MilsPatel <milanpatel2357@gmail.com>
2024-09-23 19:45:06 -0400

    First Commit

---
+++ reactMilan/src/NewComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NewComponent = () => {
+    return (
+        <div>
+            <h1>This is a new feature component!</h1>
+            <p>This component demonstrates how to add a new feature in React.</p>
+        </div>
+    );
+};
+
+export default NewComponent;
